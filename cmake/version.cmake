bda4b983c694fc5de23414d19b85b9b47b8c0829
Antoine Pollenus <a.pollenus@deltacast.tv>
2023-11-08 15:57:58 +0100

    🐛 Fix version retrieval.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -4,32 +4,31 @@ function(git_version _var)
 		execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always
 			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 			OUTPUT_VARIABLE out
+			RESULT_VARIABLE ret
+			ERROR_QUIET
 			OUTPUT_STRIP_TRAILING_WHITESPACE)
-			
-		 set(${_var}
-			  "${out}"
-			  PARENT_SCOPE)
+		
+		if(ret AND NOT ret EQUAL 0)
+			set(${_var}
+			"0.0.0"
+			PARENT_SCOPE)
+		else()
+			set(${_var}
+			"${out}"
+			PARENT_SCOPE)
+		endif()
+		 
 	endif()
 endfunction()
 
 
 function(set_version _var)
-	# Get partial versions into a list
-	string(REGEX MATCHALL "-.*$|[0-9]+" PROJECT_PARTIAL_VERSION_LIST
-		${_var})
-
-	# Set the version numbers
-	list(GET PROJECT_PARTIAL_VERSION_LIST
-		0 PROJECT_VERSION_MAJOR)
-	list(GET PROJECT_PARTIAL_VERSION_LIST
-		1 PROJECT_VERSION_MINOR)
-	list(GET PROJECT_PARTIAL_VERSION_LIST
-		2 PROJECT_VERSION_PATCH)
-
-	# Unset the list
-	unset(PROJECT_PARTIAL_VERSION_LIST)
-	
-	# Set project version (without the preceding 'v')
-	set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH} PARENT_SCOPE)
-
+    # Get partial versions into a list
+    string(REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)(.*)" VERSION_MATCHED "${_var}")
+    
+    if(VERSION_MATCHED)
+        set(PROJECT_VERSION ${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3} PARENT_SCOPE)
+    else()
+		set(PROJECT_VERSION 0.0.0 PARENT_SCOPE)
+    endif()
 endfunction()
